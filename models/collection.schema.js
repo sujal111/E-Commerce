f00cb5f33c13ff554ed33e3bc58064506c6fad78
sujalmaiti <sujalmaiti123456@gmail.com>
2023-01-17 00:52:03 +0530

    Collection schema completed

---
+++ models/collection.schema.js
@@ -0,0 +1,21 @@
+import mongoose  from "mongoose";
+
+const collectionSchema= new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:[true,"Please provide a category name"],
+            trim:true,
+            maxLength:[120,"Collection should not be more than 120 characters"]
+
+
+        },
+        
+    },
+    {
+        timestamps:true
+    }
+
+);
+
+export default mongoose.model("Collection", collectionSchema)
