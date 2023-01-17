0a720d48f2c14e857f9c11278a35cf9b13795f02
sujalmaiti <sujalmaiti123456@gmail.com>
2023-01-17 23:36:45 +0530

    morgan and other middleware added. Cors also added

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 const app=express()
+
+
+
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.use(cookieParser())
+
+
+//Morgan Logger
+app.use(morgan('tiny'))
 
 
 export default app
