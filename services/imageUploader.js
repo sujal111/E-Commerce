308183c36193c215b505de011e0f74b2cb283948
sujalmaiti <sujalmaiti123456@gmail.com>
2023-01-25 09:01:43 +0530

    s3 bucket for images

---
+++ services/imageUploader.js
@@ -0,0 +1,24 @@
+import s3 from '../config/s3.config.js'
+
+
+
+
+export const s3FileUpload=async({bucketName,key,body})=>{
+    return await s3.upload({
+        Bucket:bucketName,
+        Key:key,
+        Body:body,
+        ContentType:ContentType
+    })
+    .promise()
+}
+
+export const deleteFile=async({bucketName,key }) =>{
+
+    return await s3.deleteObject({
+        Bucket:bucketName,
+        Key:key,
+
+    })
+    .promise()
+}
